5feb81303211eee7cf3c05712985b99f616462a3
Dmytro Kozak <kozakdmitro15@gmail.com>
2022-12-24 00:49:11 +0200

    added sending messages history

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,17 +1,18 @@
 import json
 import random
 
-from channels.generic.websocket import AsyncWebsocketConsumer, AsyncJsonWebsocketConsumer
-from channels.auth import login
-from channels.db import database_sync_to_async
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
+from rest_framework.renderers import JSONRenderer
 
-from operations_with_db import save_message
+from chat_api.serializers import MessageSerializer
+from operations_with_db import save_message, get_chat_history
 
 
 class ChatConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
         self.scope["session"]["seed"] = random.randint(1, 1000)
 
+        self.channel_layer.send(self.channel_name, {"history": get_chat_history()})
         self.group_name = "chat"
         await self.channel_layer.group_add(self.group_name, self.channel_name)
 
@@ -24,15 +25,21 @@ class ChatConsumer(AsyncJsonWebsocketConsumer):
         data_json = json.loads(text_data)
         message = data_json["message"]
         username = data_json["username"]
-        save_message()
+        timestamp = await save_message(message=message, username=username)
         await self.channel_layer.group_send(self.group_name,
                                             {"type": "chat_message",
                                              "message": message,
-                                             "username": username}
+                                             "username": username,
+                                             "timestamp": timestamp}
         )
-
 
     async def chat_message(self, event):
         message = event["message"]
         username = event["username"]
         await self.send_json(content={"message": message, "username": username})
+
+    async def chat_history(self, event):
+        history = event["history"]
+        serializer = MessageSerializer(history)
+        json_history = JSONRenderer().render(serializer.data)
+        await self.send(text_data={"history": json_history})
--- chat/operations_with_db.py
+++ chat/operations_with_db.py
@@ -1,9 +1,16 @@
 from asgiref.sync import sync_to_async
+from django.utils import timezone
 
 from chat_api.models import Message
 
 
-@sync_to_async
+@sync_to_async()
 def save_message(username, message):
     message = Message(username=username, message=message)
     message.save()
+    return message.timestamp
+
+
+@sync_to_async()
+def get_chat_history():
+    return Message.objects.filter(timestamp__lte=timezone.now()).order_by('-timestamp')[:20]
