9c059717f0fb800d667024485cebadd7fa08ea2c
Dmytro Kozak <kozakdmitro15@gmail.com>
2022-12-25 01:06:37 +0200

    small changes in consumers file

--- chat/operations_with_db.py
+++ chat/operations_with_db.py
@@ -1,10 +1,12 @@
 from asgiref.sync import sync_to_async
 from django.utils import timezone
+from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 
 from chat_api.models import Message
 from chat_api.serializers import MessageSerializer
 
+import io
 
 @sync_to_async()
 def save_message(username, message):
@@ -17,4 +19,6 @@ def get_chat_history():
     history = Message.objects.filter(timestamp__lte=timezone.now()).order_by('-timestamp')[:20]
     serializer = MessageSerializer(history, many=True)
     json_history = JSONRenderer().render({"history": serializer.data})
-    return json_history
+    stream = io.BytesIO(json_history)
+    data = JSONParser().parse(stream)
+    return data
